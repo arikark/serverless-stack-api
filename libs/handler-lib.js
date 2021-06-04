7b22cf6b64f2a746ce97774bde4b1b6ed83856b6
Ariel Kark <ariel.kark@versent.com.au>
2021-06-04 14:12:08 +1000

    Adding CORS to our Serverless API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -7,15 +7,19 @@ export default function handler(lambda) {
       body = await lambda(event, context);
       statusCode = 200;
 		} catch (e) {
-			console.log(e);
+			// console.log(e);
       body = { error: e.message };
       statusCode = 500;
     }
 
     // Return HTTP response
-    return {
-      statusCode,
-      body: JSON.stringify(body),
-    };
+		return {
+			statusCode,
+			body: JSON.stringify(body),
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Credentials": true,
+			},
+		};
   };
 }
